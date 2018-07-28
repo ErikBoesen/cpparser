a1fd6d0cdaedff1354c3212bc7985741d3c22413
ErikBoesen <me@erikboesen.com>
2018-07-27 22:36:53 -0400

    download.py: print all data

--- download.py
+++ download.py
@@ -9,4 +9,11 @@ URL = 'http://www.uscyberpatriot.org/competition/current-competition/scores'
 #raw = requests.get(URL).content
 raw = open('test_data/Scores.html').read()
 soup = BeautifulSoup(raw, 'html.parser')
-print(soup.find_all('ul', class_='dfwp-column dfwp-list')[0].prettify())
+rounds = []
+main_list = soup.find('ul', class_='dfwp-column dfwp-list')
+print(len(main_list.find_all('li', class_='dfwp-item', recursive=False)))
+for rnd in main_list.find_all('li', class_='dfwp-item', recursive=False):
+    title = rnd.find('div', class_='groupheader item medium').text
+    for lnk in rnd.find_all('a'):
+        print('{name} /// {url}'.format(name=lnk.text, url=lnk['href']))
+    print('-=' * 40)
