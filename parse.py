afd6dd0fc895fff935bd69bb19dd00d857c4bd80
ErikBoesen <me@erikboesen.com>
2018-07-27 21:05:52 -0400

    Gather data more elegantly

--- parse.py
+++ parse.py
@@ -5,7 +5,7 @@ import argparse
 
 parser = argparse.ArgumentParser(description='extract important information from CyberPatriot score dumps.')
 parser.add_argument('file', help='name of/path to spreadsheet for parsing (typically in XLSX format)')
-parser.add_argument('--teams', nargs='+', help='names of teams to focus on')
+parser.add_argument('--teams', dest='teams', nargs='+', help='names of teams to focus on')
 args = parser.parse_args()
 
 wb = load_workbook(filename=args.file, read_only=True)
@@ -14,17 +14,12 @@ ws = wb.active
 rows = ws.rows
 
 fields = []
-data_reached = False
-data = []
+teams = []
 for row in rows:
-    if not data_reached:
+    if not fields:  # if fields have not yet been determined
         if row[0].value == 'Team #':
             # This is the header row
-            for cell in row:
-                fields.append(cell.value)
-            data_reached = True
+            fields = [cell.value for cell in row]
     else:
-        team = {fields[col]: cell.value for col, cell in enumerate(row)}
+        teams.append({fields[col]: cell.value for col, cell in enumerate(row)})
 
-print(fields)
-print(data)
