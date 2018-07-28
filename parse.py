fb19e7ae74f44c711bd67cd2f49488a26c973e26
ErikBoesen <me@erikboesen.com>
2018-07-27 20:36:26 -0400

    Add --teams flag

--- parse.py
+++ parse.py
@@ -5,6 +5,7 @@ import argparse
 
 parser = argparse.ArgumentParser(description='extract important information from CyberPatriot score dumps.')
 parser.add_argument('file', help='name of/path to spreadsheet for parsing (typically in XLSX format)')
+parser.add_argument('--teams', nargs='+', help='names of teams to focus on')
 args = parser.parse_args()
 
 wb = load_workbook(filename=args.file, read_only=True)
@@ -14,5 +15,5 @@ rows = ws.rows
 
 for row in rows:
     for cell in row:  # row is a tuple
-        print(cell.value, end='\t')
+        print(cell.value, end='\t | ')
     print()
