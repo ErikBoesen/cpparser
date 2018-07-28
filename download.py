668a3180fcd759e0e76e82142235f95aeea95632
ErikBoesen <me@erikboesen.com>
2018-07-28 16:17:49 -0400

    Save to output file

--- download.py
+++ download.py
@@ -4,9 +4,10 @@ import requests
 from bs4 import BeautifulSoup
 from pick import pick
 
-URL = 'http://www.uscyberpatriot.org/competition/current-competition/scores'
+SOURCE = 'http://www.uscyberpatriot.org/competition/current-competition/scores'
+OUTPUT_DIR = '/tmp'
 
-#raw = requests.get(URL).content
+#raw = requests.get(SOURCE).content
 raw = open('test_data/Scores.html').read()
 # BeautifulSoup is actually a god-breathed piece of software
 # Also it's 22:48 right now
@@ -21,4 +22,10 @@ rounds = {
 
 rnd = pick(list(rounds.keys()), title='Pick a round to get data on.')[0]
 div = pick(list(rounds[rnd].keys()), title='Pick a division to get data on.')[0]
-print(rounds[rnd][div])
+url = rounds[rnd][div]
+# TODO: There's likely a better way (read: an existing method) to remove the space escape codes.
+outfile = url.split('/')[-1].replace('%20', ' ')
+print('Downloading ' + outfile + ' from ' + url)
+# TODO: Check if requests has a better method for downloading files
+with open(OUTPUT_DIR + '/' + outfile, 'w') as f:
+    f.write(requests.get(url).content)
